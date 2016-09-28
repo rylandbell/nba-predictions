e1de6a2bb42efe5e803d72e7e09a7ca80ae60c5c
Ryland Bell <ryland.bell@gmail.com>
2016-09-28 10:39:17 -0700

    trying to fix static assets bug

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 require('dotenv').load();
+process.env.PWD = process.cwd()
+
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
@@ -26,7 +28,8 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+// app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(process.env.PWD + '/public'));
 
 app.use(passport.initialize());
 
