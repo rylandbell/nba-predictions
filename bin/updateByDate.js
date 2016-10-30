516b2e2b976372851d180c262ddf9ab037a0961e
Ryland Bell <ryland.bell@gmail.com>
2016-10-29 17:01:51 -0700

    text edits

--- bin/updateByDate.js
+++ bin/updateByDate.js
@@ -5,10 +5,16 @@ const moment = require('moment');
 const GameData = require('./modules/manage-game-data.js');
 const UserData = require('./modules/manage-user-data.js');
 
-console.log('updateByDate ran at ', moment().format('YYYY-MM-DD kk:mm'));
+console.log('updateByDate ran at UTC: ', moment().format('YYYY-MM-DD kk:mm'));
 
-const today = moment().format('YYYY-MM-DD');
+//task should run at 12AM Pacific
+//subtract 9 hours from UTC to arrive at a time that is at the end of the recently completed day, which is called 'today' 
+const today = moment().subtract(9,'hours').format('YYYY-MM-DD');
+
+//run updateData on yesterday's games for redundancy
 const yesterday = moment(today).subtract(1,'days').format('YYYY-MM-DD');
+
+//run updateData on tomorrow's games to catch schedule changes
 const tomorrow = moment(today).add(1,'days').format('YYYY-MM-DD');
 console.log('About to update data for ', yesterday, today, tomorrow);
 
