919723dabbe851d0cf46a5f62b1d46de4237bfc6
Ryland Bell <ryland.bell@gmail.com>
2016-11-01 10:32:54 -0700

    debugging bad end-of-month scoring

--- bin/updateByDate.js
+++ bin/updateByDate.js
@@ -9,6 +9,7 @@ console.log('updateByDate ran at UTC: ', moment().format('YYYY-MM-DD kk:mm'));
 
 //used to update a single day's game and user data, as scores become available
 const updateGameAndUserData = (date) => {
+  console.log('running updateGameAndUserData for ', date)
   GameData.updateSingleDate(date, UserData.markResults.bind(this, date));
 };
 
@@ -19,12 +20,12 @@ if (process.argv.length>2){
   const today = moment().subtract(9, 'hours').format('YYYY-MM-DD');
 
   //run updateData on yesterday's games for redundancy
-  const yesterday = moment(today).subtract(1, 'days').format('YYYY-MM-DD');
+  // const yesterday = moment(today).subtract(1, 'days').format('YYYY-MM-DD');
 
   //run updateData on tomorrow's games to catch schedule changes
   const tomorrow = moment(today).add(1, 'days').format('YYYY-MM-DD');
 
-  updateGameAndUserData(yesterday);
+  // updateGameAndUserData(yesterday);
   updateGameAndUserData(today);
   updateGameAndUserData(tomorrow);
 }
