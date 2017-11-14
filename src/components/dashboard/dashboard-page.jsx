089797b0fcaa166444a5bc7eef9b4fd4265001a5
Ryland Bell <ryland.bell@gmail.com>
2017-11-14 12:37:54 -0800

    re-direct to dashboard page after joining/creating first league

--- src/components/dashboard/dashboard-page.jsx
+++ src/components/dashboard/dashboard-page.jsx
@@ -1,7 +1,6 @@
 'use strict';
 
 import React from 'react';
-import Link from 'react-router/lib/Link';
 
 import StandingsContainer from '../containers/standings-container.jsx';
 import PicksSummaryContainer from '../containers/picks-summary-container.jsx';
@@ -14,10 +13,6 @@ const DashboardPage = React.createClass({
   render: function() {
     return (
       <div>
-        <p className="text-center dashboard-help-text"> Not sure what's happening? Need clarification about the rules? Check out the&nbsp;
-          <Link to="/how-to-play">How to Play</Link>
-          &nbsp;page.
-        </p>
         <div className="row full-height-parent">
           <div className="col-xs-12 col-sm-6 col-md-5 col-lg-5">
             <PicksSummaryContainer />
