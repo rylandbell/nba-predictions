d7834ec42e6ed58e8aca6594a1671948cadd009f
Ryland Bell <ryland.bell@gmail.com>
2017-11-17 16:24:12 -0800

    make dashboard tour check screen size

--- src/intro-tours.js
+++ src/intro-tours.js
@@ -1,31 +1,60 @@
 import {introJs} from 'intro.js';
 import actions from './actions/action-creators.js';
 
+// tour steps for SM-LG windows:
+const regSteps = [
+  { 
+    intro: "You're almost ready to start playing, but let's start with a quick tour."
+  },
+  {
+    element: document.querySelector('.navbar-brand'),
+    intro: "Wherever you are on the site, you can always click the Pigeon Hoops logo to return to this page."
+  },
+  {
+    element: document.querySelector('.more-dropdown'),
+    intro: "The \"More\" menu contains links to two important pages. The Leagues page lets you create a new league, join a friend's league, and view info about your current league(s). And the How-to-Play page explains the rules of the game."
+  },
+  {
+    element: document.querySelector('.league-month-picker'),
+    intro: "Here you can change the league and month for which you\'re making picks and viewing results."
+  },
+  {
+    element: document.querySelector('.picks-summary'),
+    intro: "Now let's make some actual picks! Click the Sign Up button to join the selected month's competition."
+  }
+]
+
+// tour steps for XS windows
+const xsSteps = [
+  { 
+    intro: "XS! You're almost ready to start playing, but let's start with a quick tour."
+  },
+  {
+    element: document.querySelector('.navbar-brand'),
+    intro: "Wherever you are on the site, you can always click the Pigeon Hoops logo to return to this page."
+  },
+  {
+    element: document.querySelector('.more-dropdown'),
+    intro: "The \"More\" menu contains links to two important pages. The Leagues page lets you create a new league, join a friend's league, and view info about your current league(s). And the How-to-Play page explains the rules of the game."
+  },
+  {
+    element: document.querySelector('.league-month-picker'),
+    intro: "Here you can change the league and month for which you\'re making picks and viewing results."
+  },
+  {
+    element: document.querySelector('.picks-summary'),
+    intro: "Now let's make some actual picks! Click the Sign Up button to join the selected month's competition."
+  }
+]
+
 export function runDashboardIntro(dispatch){
+
+  const xsWindow = window.innerWidth < 768;
+
   window.dashboardIntro = introJs();
   
   window.dashboardIntro.setOptions({
-    steps: [
-      { 
-        intro: "You're almost ready to start playing, but let's start with a quick tour."
-      },
-      {
-        element: document.querySelector('.navbar-brand'),
-        intro: "Wherever you are on the site, you can always click the Pigeon Hoops logo to return to this page."
-      },
-      {
-        element: document.querySelector('.more-dropdown'),
-        intro: "The \"More\" menu contains links to two important pages. The Leagues page lets you create a new league, join a friend's league, and view info about your current league(s). And the How-to-Play page explains the rules of the game."
-      },
-      {
-        element: document.querySelector('.league-month-picker'),
-        intro: "Here you can change the league and month for which you\'re making picks and viewing results."
-      },
-      {
-        element: document.querySelector('.picks-summary'),
-        intro: "Now let's make some actual picks! Click the Sign Up button to join the selected month's competition."
-      }
-    ],
+    steps: xsWindow ? xsSteps : regSteps,
     showProgress: true,
     showStepNumbers: false,
     nextLabel: " > ",
