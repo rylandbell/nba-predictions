d419555efddc3fa16a8612c52cb9be7e6504b776
Ryland Bell <ryland.bell@gmail.com>
2017-11-21 14:03:13 -0800

    delete dead CSS code

--- src/selectors/noLeaguesJoined.js
+++ src/selectors/noLeaguesJoined.js
@@ -4,6 +4,7 @@ const getUser = state => state.apiData.user;
 
 // Should return true if user does NOT have any joined leagues
 export const checkNoLeaguesJoined = createSelector([getUser], user => {
+  console.log(user.leagues);
   if (user.leagues && user.leagues.length < 1) {
     return true;
   } else {
